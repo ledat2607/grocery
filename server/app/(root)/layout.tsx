5dcefbbc91ff42e97327618ebc934838af25f61f
LEDAT\ledat <ledat260701@gmail.com>
2024-09-28 03:51:24 +0700

    1

---
+++ server/app/(root)/layout.tsx
@@ -0,0 +1,5 @@
+const SetupLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export default SetupLayout;
---
+++ server/app/(root)/page.tsx
@@ -0,0 +1,5 @@
+const SetupnPage = () => {
+    return <div>Setup page</div>;
+}
+ 
+export default SetupnPage;
