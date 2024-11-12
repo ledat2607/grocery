8957bb869a51bfa0e3a48760ee92a32fff08562f
ledat2607 <76934823+ledat2607@users.noreply.github.com>
2024-11-12 12:33:00 +0700

    Update const.ts

--- server/const.ts
+++ server/const.ts
@@ -1,22 +1,29 @@
 // constants.js
 
 export const UserRoles = [
-  // {
-  //   role: "admin",
-  //   id: "user_2nTaRSY4umnwSd7VEHEeQdTHqG5", // Replace with the actual admin ID
-  // },
   {
     role: "admin",
-    id: "user_2lomsDMK4oxfZUkHjVPu7qWl2Fu", // Replace with the actual admin ID
+    id: "user_2nTaRSY4umnwSd7VEHEeQdTHqG5", // Replace with the actual admin ID
+  },
+  {
+    role: "admin",
+    id: "user_2ojcsnIqOVywZunIDjWNnUELX9j",
+    //username: hoanganh
+    //pass:12345678
+    // Replace with the actual admin ID
   },
   {
     role: "shipper",
     id: "user_2lomsDMK4oxfZUkHjVPu7qWl2Fu", // Replace with the actual admin ID
   },
- 
+  {
+    role: "shipper",
+    id: "user_2ojcxRZfiaYfti7tJZ8S7QdIOYW",
+    //username: hoanganhngo
+    //pass:12345678 // Replace with the actual admin ID
+  },
   {
     role: "shipper",
     id: "user_2nQmmQEk8AXgN0zTD1aCoouW0KS", // Replace with the actual shipper ID
   },
 ];
-  
